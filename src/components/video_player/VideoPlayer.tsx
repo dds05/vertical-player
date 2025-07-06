a066f7108016e1cc6386539272cdc55cf00502ff
Damandeep Singh <damandeep.in@gmail.com>
2025-07-06 18:42:57 +0530

    refactor: VideoPlayer

--- src/components/video_player/VideoPlayer.tsx
+++ src/components/video_player/VideoPlayer.tsx
@@ -5,6 +5,7 @@ import videojs from "video.js";
 import "video.js/dist/video-js.css";
 import './VideoPlayer.css';
 import { initaliseSkin } from "../skin";
+import { PlayerInstance } from "../types/videojs";
 
 interface VideoPlayerProps {
   videoSrc: { src: string; type: string };
@@ -14,33 +15,30 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoSrc, playerId, playing = true }) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
-  const playerRef = useRef<videojs.Player | null>(null);
+  const playerRef = useRef<PlayerInstance | null >(null);
 
   useEffect(() => {
 
-        if (videoRef.current && !playerRef.current) {
-            console.log(videoRef.current,playerId);
-            
-            playerRef.current = videojs(videoRef.current, {
-              autoplay: false,
-              controls: false,
-              playerId :playerId,
-              muted: true,
-              loop: true,
-              preload: "auto",
-              
-              aspectRatio: '9:16',
-              sources: [videoSrc],
-            },()=>{
-              initaliseSkin(playerId,{})
-            });
-          }
+    if (videoRef.current && !playerRef.current) {
+      console.log(videoRef.current, playerId);
+      playerRef.current = videojs(videoRef.current, {
+        autoplay: false,
+        controls: false,
+        playerId: playerId,
+        muted: true,
+        loop: true,
+        preload: "auto",
+        aspectRatio: '9:16',
+        sources: [videoSrc],
+      }, () => {
+        initaliseSkin(playerId, {})
+      });
+    }
 
 
 
     return () => {
       if (playerRef.current) {
-        // playerRef.current.dispose();
         playerRef.current = null;
       }
     };
@@ -50,44 +48,36 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoSrc, playerId, playing =
     if (!playerRef.current) return;
 
     if (playing) {
-      playerRef.current.play().catch((err) => {
+      handleSoundAction();
+      playerRef?.current?.play()?.catch((err) => {
         console.warn("Autoplay blocked or failed:", err);
       });
     } else {
       playerRef.current.pause();
+      playerRef.current.currentTime(0)
     }
   }, [playing]);
+  
 
-  useEffect(() =>{
- 
-      if (playerRef.current) {
-        if (playing) {
-          handleSoundAction();
-          playerRef.current.play();
-        } else {
-          playerRef.current.pause();
-        }
-      }
-  },
-  [playing]);
-  
-  const handleSoundAction = ()=>{
-    let muted = sessionStorage.getItem('vertical_player_volume');
+  const handleSoundAction = () => {
+    let muted: number = parseInt(sessionStorage.getItem('vertical_player_volume')|| '0');
     if (muted == 0 || muted == null) {
-      playerRef.current.muted(true)
+      playerRef?.current?.muted(true)
     } else {
-      if (playerRef.current.muted()) playerRef.current.muted(false) 
-        if (muted) {
+      if (playerRef?.current?.muted()) 
+        playerRef.current.muted(false)
+
+      if (muted) {
         let vol = parseFloat(muted) / 100.0;
-        playerRef.current.volume(vol);
+        playerRef?.current?.volume(vol);
       }
-  
+
     }
   }
 
   return (
     <div className="vertical-player-wrapper">
-      {  <video
+      {<video
         ref={videoRef}
         id={playerId}
         className="video-js vjs-big-play-centered"
